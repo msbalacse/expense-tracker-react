0992b4365f7c84e2fa2ed07f0279ea99f5302855
Balamurugan <msbalacse@gmail.com>
2023-11-29 22:28:00 +0530

    updated

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,3 +1,4 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -6,8 +7,29 @@ const UserProfile = () => {
   console.log(user);
   return (
     <div>
-      usermail : {user.email}
-      <br /> {user.uid}
+      <Formik
+        initialValues={{ name: '', email: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => {
+          return (
+            <Form className="flex flex-col gap-2">
+              <Field type="text" name="name" />
+              <ErrorMessage name="name" component="div" />
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                save
+              </button>
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 };
